2fc93a111dca6f975aab5041c563b3a26ba3b11e
rivening <149218982+rivening@users.noreply.github.com>
2023-11-03 23:38:17 +0900

    [level unrated] Title: 소인수분해, Time: 0.05 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        
+         List<Integer> list = new ArrayList<>();
+        int idx = 0;
+        
+        for (int i=2 ; i<=n; i++){
+            
+            if (n % i == 0){
+                list.add(i);
+            }
+            
+            while (n % i == 0) {
+                n /= i;
+            }
+
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i=0; i<answer.length; i++){
+            answer[i] += list.get(i) ;
+        }
+            
+        
+
+        return answer;
+    }
+}
