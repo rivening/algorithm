0149dd5acf466c65f26084ed8ba3eddb6e270a34
rivening <149218982+rivening@users.noreply.github.com>
2023-11-04 00:55:32 +0900

    [level unrated] Title: 소인수분해, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.java
@@ -30,3 +30,13 @@ class Solution {
         return answer;
     }
 }
+
+// class Solution {
+//     public String solution(String cipher, int code) {
+//         String answer = "";
+//         for(int i=code-1; i<cipher.length(); i+=code){
+//             answer += cipher.substring(i, i+1);
+//         }
+//         return answer;
+//     }
+// }
